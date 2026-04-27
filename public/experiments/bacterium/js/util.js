83c30b5c1131df2b2e1a5341cf4d9bb92f4e5273
LZC <lzc@LZCdeMacBook-Pro.local>
2026-04-27 16:25:30 +0800

    fix: add missing Point class and Region class to bacterium/util.js (Point isNotDefined error)

--- public/experiments/bacterium/js/util.js
+++ public/experiments/bacterium/js/util.js
@@ -14,3 +14,41 @@ var UserProfile = {
     }
   }
 };
+
+/**
+ * Simple 2D Point class
+ */
+function Point( x, y ) {
+  this.x = x || 0;
+  this.y = y || 0;
+}
+
+Point.prototype.distanceTo = function( target ) {
+  var dx = this.x - (target.x || 0);
+  var dy = this.y - (target.y || 0);
+  return Math.sqrt( dx * dx + dy * dy );
+};
+
+/**
+ * Axis-aligned bounding rectangle
+ */
+function Region() {
+  this.left = 0;
+  this.right = 0;
+  this.top = 0;
+  this.bottom = 0;
+}
+
+Region.prototype.reset = function() {
+  this.left =  Infinity;
+  this.right = -Infinity;
+  this.top  =  Infinity;
+  this.bottom = -Infinity;
+};
+
+Region.prototype.inflate = function( x, y ) {
+  if( x < this.left )   this.left = x;
+  if( x > this.right )  this.right = x;
+  if( y < this.top )    this.top = y;
+  if( y > this.bottom ) this.bottom = y;
+};
